114bfd6889d1398760d6e0ec853f49536314514f
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-06 03:59:31 +0300

    First Exercise determine if the second sequence passed into the TestForSquares() method contains the squares of the elements in the first sequence

---
+++ Exercise1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercises
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            List<int> squares = new List<int> { 1, 4, 9, 16 };
+
+            var function = TestForSquares(numbers, squares);
+            Console.WriteLine(function);
+        }
+        public static bool TestForSquares(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            return numbers.Select(number => (int)Math.Pow(number,2)).SequenceEqual(squares);
+
+        }
+    }
+}
+
+
+   
