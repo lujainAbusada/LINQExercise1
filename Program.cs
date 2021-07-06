a38ed8bdb82fa49369b2b14ed8a71ab16ac132e2
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-06 03:42:23 +0300

    This project contains solutios for LINQ exercises

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
